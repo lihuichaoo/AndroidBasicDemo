0c36e9ed6db34a98f61f7c7d2e9015a3951e8c4f
aochuih <aochuih@hotmail.com>
2019-05-19 18:20:49 +0800

    add flutter view

--- app/src/main/java/com/example/basicdemo/MainActivity.java
+++ app/src/main/java/com/example/basicdemo/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +25,17 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                View flutterView = Flutter.createView(
+                        MainActivity.this,
+                        getLifecycle(),
+                        "route1"
+                );
+                FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(600, 800);
+                layout.leftMargin = 100;
+                layout.topMargin = 200;
+                addContentView(flutterView, layout);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
